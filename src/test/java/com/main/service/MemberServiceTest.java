0f875c610f8f0ded018411b04802fe59028619c6
jun <an0yjun@naver.com>
2022-03-07 21:48:06 +0900

    .

--- src/test/java/com/main/service/MemberServiceTest.java
+++ src/test/java/com/main/service/MemberServiceTest.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.test.context.BootstrapWith;
 
 import com.main.domain.Member;
 
@@ -24,7 +25,26 @@ class MemberServiceTest {
 		Member findMember = memberService.findeOne(saveId).get();
 		Assertions.assertThat(member.getName()).isEqualTo(findMember.getName());
 	}
-
+	
+	@Test
+	public void validateDuplicateMember() {
+		//given
+		Member member1 = new Member();
+		member1.setName("spring");
+		
+		Member member2= new Member();
+		member2.setName("spring");
+		
+		//when
+		memberService.join(member1);
+		
+		try {
+			memberService.join(member2);
+			fail("중복 예외발생");
+		} catch (IllegalStateException e) {
+			// TODO: handle exception
+		}
+	}
 	@Test
 	void testFindMembers() {
 		fail("Not yet implemented");
